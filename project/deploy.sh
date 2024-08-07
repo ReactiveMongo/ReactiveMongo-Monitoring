6a39ff22b04101aa09dc490db6909dfa0bf755f2
Cédric Chantepie <cchantep@users.noreply.github.com>
2024-08-07 17:44:38 +0200

    Release candidate 1.1.0-RC13

--- project/deploy.sh
+++ project/deploy.sh
@@ -42,7 +42,7 @@ EOF
 }
 
 SCALA_MODULES="jmx:reactivemongo-jmx kamon:reactivemongo-kamon datadog:reactivemongo-datadog"
-SCALA_VERSIONS="2.11 2.12 2.13 3.2.2"
+SCALA_VERSIONS="2.11 2.12 2.13 3.4.2"
 BASES=""
 
 for V in $SCALA_VERSIONS; do
